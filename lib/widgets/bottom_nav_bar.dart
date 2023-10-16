ee70bb6f9ad4e361072ff9a4b00d47f5e2560d24
FLopriore <115494520+FLopriore@users.noreply.github.com>
2023-10-16 20:04:10 +0200

    Add navigation bar

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+/*
+* This widget is a navigation bar shown on the bottom of the screen.
+* You can select one of these pages:
+*   - Home (default)
+*   - Reminder
+*   - My orders
+* */
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int currentPageIndex = 1; // default page is home
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        backgroundColor: const Color(0xffa6e79b), // TODO: use unified theme instead of hardcoded colors
+        indicatorColor: const Color(0xff91d285),
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.access_time_outlined),
+            label: 'Promemoria',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.local_shipping),
+            icon: Icon(Icons.local_shipping_outlined),
+            label: 'I miei ordini',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        Placeholder(), // TODO: add "reminder" widget
+        Placeholder(), // TODO: add "home" widget
+        Placeholder(), // TODO: add "my orders" widget
+      ][currentPageIndex],
+    );
+  }
+}
