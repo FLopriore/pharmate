3a90191cc00f09638eea5545c671e036a6eade05
FLopriore <115494520+FLopriore@users.noreply.github.com>
2023-10-20 23:19:11 +0200

    Remove search anchor

--- lib/widgets/custom_search_bar.dart
+++ lib/widgets/custom_search_bar.dart
@@ -1,51 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:pharmate/screens/my_orders.dart';
 
 class CustomSearchBar extends StatefulWidget {
   const CustomSearchBar({super.key});
 
   @override
-  State<CustomSearchBar> createState() => _SearchBarAppState();
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
 }
 
-class _SearchBarAppState extends State<CustomSearchBar> {
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 20,top: 5,left: 20),
-      child:SearchAnchor(
-          builder: (BuildContext context, SearchController controller) {
-        return SearchBar(
-          controller: controller,
-          hintText: "Cerca un Farmaco",
-          padding: const MaterialStatePropertyAll<EdgeInsets>(
-              EdgeInsets.symmetric(horizontal: 10.0)),
-          onTap: () {
-            controller.openView();
-          },
-          onChanged: (_) {
-            controller.openView();
-          },
-          onSubmitted: (String str){
-            Navigator.of(context).push(MaterialPageRoute(builder:(context) => const MyOrders()));
-          },
-          leading: const Icon(Icons.search),
-        );
-      }, suggestionsBuilder:
-              (BuildContext context, SearchController controller) {
-        return List<ListTile>.generate(3, (int index) {  // TODO: change size of list
-          final String item = 'item $index';
-          return ListTile(
-            title: Text(item),
-            onTap: () {
-              setState(() {
-                controller.closeView(item); // TODO: read medicines from db
-              });
-            },
-          );
-        });
-      }),
+      padding: const EdgeInsets.only(right: 20, top: 5, left: 20),
+      child: SearchBar(
+        controller: controller,
+        hintText: "Cerca un farmaco da ordinare",
+        padding: const MaterialStatePropertyAll<EdgeInsets>(
+            EdgeInsets.symmetric(horizontal: 10.0)),
+        onSubmitted: (String str) {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => const MyOrders()));
+        },
+        leading: const Icon(Icons.search),
+      ),
     );
   }
 }
