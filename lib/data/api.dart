3743b082b02d15234f3ef78f2aafe73089d1fc54
FLopriore <115494520+FLopriore@users.noreply.github.com>
2023-11-26 21:40:45 +0100

    Create method to GET data from the server

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -24,4 +24,23 @@ class CallApi {
       return null;
     }
   }
+
+  // Get
+  Future<dynamic> getData(String apiUrl) async {
+    String fullUrl = _url + apiUrl;
+    HttpClient client = HttpClient();
+    // Bypass SSL certification
+    client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+
+    HttpClientRequest request = await client.getUrl(Uri.parse(fullUrl));
+    request.headers.set('Content-Type', 'application/json');
+    request.headers.set('authorization', 'Bearer cicciopasticcio1920');
+    HttpClientResponse result = await request.close();
+
+    if (result.statusCode == 200) {
+      return jsonDecode(await result.transform(utf8.decoder).join());
+    } else {
+      return null;
+    }
+  }
 }
