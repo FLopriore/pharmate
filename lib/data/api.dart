6d24b5eff57d369eee895782d8f798387006a4ca
FrancescoDesiderato <desiderato.francescosalvatore@gmail.com>
2023-12-15 15:03:43 +0100

    Add delete method to api class

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -50,4 +50,28 @@ class CallApi {
     }
     return null;
   }
-}
+
+  //DELETE
+  Future<dynamic> deleteData(data, String apiUrl) async {
+    String fullUrl = _url + apiUrl;
+    HttpClient client = HttpClient();
+    // Bypass SSL certification
+    client.badCertificateCallback =
+        ((X509Certificate cert, String host, int port) => true);
+
+    HttpClientRequest request = await client.deleteUrl(Uri.parse(fullUrl));
+    request.headers.set('Content-Type', 'application/json');
+    request.headers.set('accept', '*/*');
+
+    String? token = await LoginSecureStorage.getLoginSecureStorage('loginToken');
+    request.headers.set('Authorization', 'Bearer ${token!}');
+    request.add(utf8.encode(jsonEncode(data)));
+    HttpClientResponse result = await request.close();
+
+    if (result.statusCode == 200) {
+      return jsonDecode(await result.transform(utf8.decoder).join());
+    } else {
+      return null;
+    }
+  }
+  }
